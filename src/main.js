dc363439d19cb1e338107ee9a5d219abe7161243
williamfalci <info@williamfalci.it>
2021-12-02 18:28:26 +0100

    added supabase

--- src/main.js
+++ src/main.js
@@ -6,10 +6,17 @@ import router from './router/index.js'
 import i18n from './i18n/i18n.js'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import supabase from 'vue-3-supabase'
 
+console.log(process.env)
 createApp(App)
 .use(i18n)
 .use(store)
 .use(router)
 .use(VueAxios, axios)
+.use(supabase, {
+  supabaseUrl: process.env.VUE_APP_SUPABASE_URL, // API URL
+  supabaseKey: process.env.VUE_APP_SUPABASE_ANON_KEY, // ANON KEY,
+  options: {}
+})
 .mount('#app')
